4dee8747f436cc49f670c8d42725ad4a83a66a69
bycrea <yoan.despert@lapiscine.pro>
2019-10-28 13:42:01 +0100

    cart front ok/no api

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import trash from '../images/trash.png'
+import trash from '../images/trash.png';
 
 class Cart extends Component {
   constructor(props) {
@@ -12,83 +12,120 @@ class Cart extends Component {
       selectedCatgId: null,
       selectedCatgName: "",
       inTrash: [],
+      countClick: 0,
       loading: true,
       error: false
     }
-    //this.textInput = React.createRef();
+    this.textInput = React.createRef();
   }
 
   componentDidMount() {
-    fetch('http://coccooncart.com/api/getlist')
-      .then(res => res.json())
-      .then((result) => {
-        console.log(result);
-        if(result.error === false) {
-          this.setState({
-            listId: result.listId,
-            list: result.list || [],
-            categories: result.categories,
-            selectedCatgId: parseInt(result.categories[0].id),
-            selectedCatgName: result.categories[0].name,
-            loading: false,
-          });
-          console.log('success')
-        } else {
-          console.log(result.error)
-          this.setState({
-            error: true,
-            loading: false,
-          });
-        }
-      },
-      (error) => {
-        this.setState({
-          error: true,
-          loading: false
-        });
-        console.log(error)
-      }
-    )
+    this.setState({
+      list: [
+        { name: "fraise", idcategory: 3 },
+        { name: "pomme", idcategory: 3 },
+        { name: "coca", checked: false, idcategory: 2 },
+        { name: "pates", checked: true, idcategory: 1 },
+        { name: "tomates", checked: false, idcategory: 3 },
+        { name: "steak haché bio", checked: false, idcategory: 5 },
+        { name: "des choses", checked: false, idcategory: 10 }
+      ],
+      categories: [
+        { id: 1, name: "Epicerie" },
+        { id: 2, name: "Boissons" },
+        { id: 3, name: "Fruit/Légume" },
+        { id: 4, name: "Produits Laitiers" },
+        { id: 5, name: "Viandes/Poissons" },
+        { id: 6, name: "Petit Déjeuné" },
+        { id: 7, name: "Surgelés" },
+        { id: 8, name: "Plats Préparés" },
+        { id: 9, name: "Entretien" },
+        { id: 10, name: "Hygiène" },
+        { id: 11, name: "Divers" },
+        { id: 12, name: "Recettes (idées)" }
+      ],
+      selectedCatgId: parseInt(1),
+      selectedCatgName: "Epicerie",
+      loading: false,
+    })
+    // fetch('http://coccooncart.com/api/getlist')
+    //   .then(res => res.json())
+    //   .then((result) => {
+    //     console.log(result);
+    //     if(result.error === false) {
+    //       this.setState({
+    //         listId: result.listId,
+    //         list: result.list || [],
+    //         categories: result.categories,
+    //         selectedCatgId: parseInt(result.categories[0].id),
+    //         selectedCatgName: result.categories[0].name,
+    //         loading: false,
+    //       });
+    //       console.log('success')
+    //     } else {
+    //       console.log(result.error)
+    //       this.setState({
+    //         error: true,
+    //         loading: false,
+    //       });
+    //     }
+    //   },
+    //   (error) => {
+    //     this.setState({
+    //       error: true,
+    //       loading: false
+    //     });
+    //     console.log(error)
+    //   }
+    // )
   }
 
+  // componentDidUpdate() {
+  //   console.log(this.textInput.current)
+  // }
+
   updateList(newList = this.state.list) {
-    fetch('http://coccooncart.com/api/updatelist', {
-      method: 'POST',
-      body: JSON.stringify({listId: this.state.listId, list: newList, amount: 0})
-     })
-      .then(res => res.json())
-      .then((result) => {
-        //console.log(this.state.list.length, result.nbProduct)
-        if(result.error === false) {
-          if(this.state.listId === null) {
-            this.setState({
-              listId: result.listId
-            });
-          }
-        } else {
-          console.log(result.error)
-          this.setState({
-            error: true
-          });
-        }
-      },
-      (error) => {
-        console.log(error)
-        this.setState({
-          error: true
-        });
-      }
-    )
+    // fetch('http://coccooncart.com/api/updatelist', {
+    //   method: 'POST',
+    //   body: JSON.stringify({listId: this.state.listId, list: newList, amount: 0})
+    //  })
+    //   .then(res => res.json())
+    //   .then((result) => {
+    //     //console.log(this.state.list.length, result.nbProduct)
+    //     if(result.error === false) {
+    //       if(this.state.listId === null) {
+    //         this.setState({
+    //           listId: result.listId
+    //         });
+    //       }
+    //     } else {
+    //       console.log(result.error)
+    //       this.setState({
+    //         error: true
+    //       });
+    //     }
+    //   },
+    //   (error) => {
+    //     console.log(error)
+    //     this.setState({
+    //       error: true
+    //     });
+    //   }
+    // )
   }
 
   handleAddProduct = () => {
-    const addProduct = {name: this.state.product, idcategory: this.state.selectedCatgId}
-    const newList = [addProduct].concat(this.state.list);
-    //console.log(addProduct);
-    this.setState({
-      list: newList,
-      product: ""
-    }, this.updateList(newList));
+    if(this.state.product.replace(/\s+/g, "") !== "")
+    {
+      const addProduct = {name: this.state.product, idcategory: this.state.selectedCatgId}
+      const newList = [addProduct].concat(this.state.list);
+      //console.log(addProduct);
+      this.setState({
+        list: newList,
+        product: ""
+      }, this.updateList(newList));
+    }
+    this.textInput.current.focus();
   }
 
   handleChange = (e) => {
@@ -123,12 +160,22 @@ class Cart extends Component {
   }
 
   handleClickOnCategory = (index) => {
-    const [id, name] = [parseInt(this.state.categories[index].id), this.state.categories[index].name]
-    if(id !== this.state.selectedCatgId)
+    const [id, name, click] = [
+      parseInt(this.state.categories[index].id), 
+      this.state.categories[index].name, 
+      this.state.countClick + 1
+    ];
+    if(id === this.state.selectedCatgId && click > 1)
     {
       this.setState({
+        countClick: 0
+      });
+      this.textInput.current.focus();
+    } else {
+      this.setState({
         selectedCatgId: id,
-        selectedCatgName: name
+        selectedCatgName: name,
+        countClick: click,
       });
     }
   }
@@ -172,38 +219,14 @@ class Cart extends Component {
             </div>
           :
             <div className="list-container">
-              <div className="row d-flex justify-content-center">
-                <div className="col col-8 col-sm-8">
-                  <input id="add-product"
-                    className="form-control form-control"
-                    name="product" 
-                    type="text" 
-                    value={this.state.product} 
-                    onChange={this.handleChange} 
-                    onKeyDown={this.handleKey} 
-                    placeholder={this.state.selectedCatgName} 
-                    // ref={this.textInput} 
-                  />
-                </div>
-                <div className="col col-3 col-sm-3 add">
-                  <input 
-                    className="btn btn btn-secondary"
-                    type="submit" 
-                    name="add" 
-                    value="Add" 
-                    onClick={this.handleAddProduct} 
-                  />
-                </div>
-              </div>
               <div className="list">
                 {this.state.categories.map(
                   (c, index) => 
                   <div key={index}>
-                    <a href="#top" onClick={this.handleClickOnCategory.bind(null, index)}>
-                      <span className={this.state.selectedCatgId === c.id ? "c-list-hover" : "c-list"}>
-                        {c.name}<hr key={index}></hr>
-                      </span>
-                    </a>
+                    <span className={this.state.selectedCatgId === c.id ? "c-list-hover" : "c-list"}
+                      onClick={this.handleClickOnCategory.bind(null, index)}>
+                      {c.name}<hr key={index}></hr>
+                    </span>
                     {this.state.list.map(
                       (p, index) => 
                         p.idcategory === c.id 
@@ -213,8 +236,7 @@ class Cart extends Component {
                               <label className="p-list-name">
                                 <input className="check" 
                                   type="checkbox" 
-                                  //hidden 
-                                  checked={p.checked} 
+                                  checked={p.checked || false} 
                                   value={index} 
                                   onChange={this.handleCheck} 
                                 />
@@ -238,17 +260,49 @@ class Cart extends Component {
                   </div>
                 )}
               </div>
-              <div className="b-list">
-                <button className="btn btn-sm btn-secondary undo" 
-                  disabled={this.state.inTrash.length <= 0}
-                  onClick={this.handleUndo}>Undo</button>
-                <button className="btn btn-sm btn-warning done" onClick={this.handleDone}>Done</button>
-                <input id="amount"
-                  className="form-control form-control-sm a-input"
-                  name="amount" 
-                  type="number" 
-                  placeholder="€"
-                />
+              <div className="footer-list">
+                <div className="row pb-2 input-product">
+                  <div className="col col-9 col-sm-9">
+                    <input
+                      className="form-control form-control-sm"
+                      name="product" 
+                      type="text" 
+                      value={this.state.product} 
+                      onChange={this.handleChange} 
+                      onKeyDown={this.handleKey} 
+                      placeholder={this.state.selectedCatgName} 
+                      ref={this.textInput} 
+                    />
+                  </div>
+                  <div className="col col-3 col-sm-3 add">
+                    <input 
+                      className="btn btn-sm btn-secondary"
+                      type="submit" 
+                      name="add" 
+                      value="Add" 
+                      onClick={this.handleAddProduct} 
+                    />
+                  </div>
+                </div>
+                <div className="row buttons">
+                  <div className="col col-3 col-sm-3">
+                    <button className="btn btn-sm btn-secondary undo" 
+                      disabled={this.state.inTrash.length <= 0}
+                      onClick={this.handleUndo}>Undo
+                    </button>
+                  </div>
+                  <div className="col col-5 col-sm-5">
+                    <button className="btn btn-sm btn-warning done" onClick={this.handleDone}>Done</button>
+                  </div>
+                  <div className="col col-3 col-sm-3">
+                    <input id="amount"
+                      className="form-control form-control-sm amount"
+                      name="amount" 
+                      type="number" 
+                      placeholder="€"
+                    />
+                  </div>
+                </div>
               </div>
             </div>
         }
--- src/components/InputProduct.js
+++ src/components/InputProduct.js
@@ -1,95 +1,26 @@
-import React, { Component } from 'react';
+list = [
+  { name: "fraise", idcategory: 3 },
+  { name: "pomme", idcategory: 3 },
+  { name: "coca", checked: false, idcategory: 2 },
+  { name: "pates", checked: true, idcategory: 1 },
+  { name: "tomates", checked: false, idcategory: 3 },
+  { name: "steak haché bio", checked: false, idcategory: 5 },
+  { name: "des choses", checked: false, idcategory: 10 }
+];
 
-class InputProduct extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      name: "",
-      idcategory: this.props.categories[0].id,
-      // list: [
-      //   {name: 'test', idcategory: 12, checked: false},
-      //   {name: 'carotte', idcategory: 1, checked: false},
-      //   {name: 'des choses', idcategory: 9, checked: true},
-      //   {name: 'des choses avec de text', idcategory: 2, checked: false},
-      //   {name: 'test', idcategory: 2, checked: false},
-      //   {name: 'pq', idcategory: 7, checked: false},
-      //   {name: 'des choses encore', idcategory: 8, checked: true},
-      //   {name: 'des choses', idcategory: 10, checked: false},
-      // ],
-    }
-    this.textInput = React.createRef();
-  }
-
-  handleChange = (e) => {
-    const [index, value] = [e.target.name, e.target.value]
-    if(index === 'idcategory'){
-      this.setState(() => ({
-        [index]: parseInt(value)
-      }));
-      this.props.updateIdcategory(parseInt(value));
-    } else {
-      this.setState(() => ({
-        [index]: value
-      }));
-    }
-    this.textInput.current.focus();
-  }
-
-  handleKey = (e) => {
-    if (e.key === 'Enter') {
-      this.handleSubmit();
-    }
-  }
-
-  handleSubmit = () => {
-    if(this.state.name) {
-      this.props.addToList(this.state)
-      this.props.updateIdcategory(this.props.idcategory);
-    }
-    this.setState({
-      name: "",
-    });
-    this.textInput.current.focus();
-  }
+catg = [
+  { id: 1, name: "Epicerie" },
+  { id: 2, name: "Boissons" },
+  { id: 3, name: "Fruit/Légume" },
+  { id: 4, name: "Produits Laitiers" },
+  { id: 5, name: "Viandes/Poissons" },
+  { id: 6, name: "Petit Déjeuné" },
+  { id: 7, name: "Surgelés" },
+  { id: 8, name: "Plats Préparés" },
+  { id: 9, name: "Entretien" },
+  { id: 10, name: "Hygiène" },
+  { id: 11, name: "Divers" },
+  { id: 12, name: "Recettes (idées)" }
+];
 
 
-  render() {
-
-    return (
-      <div className="row">
-        <div className="col col-5 col-sm-5">
-          <input id="add-product"
-            className="form-control form-control-sm"
-            name="name" 
-            type="text" 
-            value={this.state.name} 
-            onChange={this.handleChange} 
-            onKeyDown={this.handleKey} 
-            placeholder="add item" 
-            ref={this.textInput} 
-          />
-        </div>
-        <div className="col col-5 col-sm-5">
-          <select id="add-categories"
-            className="form-control form-control-sm"
-            name="idcategory" 
-            value={this.props.idcategory || this.state.idcategory} 
-            onChange={this.handleChange}>
-            {this.props.categories.map((c) => <option value={c.id} key={c.id}>{c.name}</option>)}
-          </select>
-        </div>
-        <div className="col col-sm">
-          <input 
-            className="btn btn-sm btn-info"
-            type="submit" 
-            name="submit" 
-            value="Add" 
-            onClick={this.handleSubmit} 
-          />
-        </div>
-      </div>
-    )
-  }
-}
-
-export default InputProduct;
